f852bb8d0d38f0b2668fec36510bc23e419cd8ad
alper <alperensurucu.eng@gmail.com>
2024-03-27 01:43:33 +0300

    Added.Index.Html

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,7 +2,7 @@
 {
     public class Product
     {
-        public int ProductI { get; set; }
+        public int ProductId { get; set; }
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public string Image { get; set; } = string.Empty;
--- Models/Repository.cs
+++ Models/Repository.cs
@@ -8,7 +8,15 @@
         static Repository()
         {
             _Categories.Add(new Category{CategoryId =1 , Name= "Telefon"}); 
-            _Categories.Add(new Category{CategoryId =2 , Name= "Bilgisayar"}); 
+            _Categories.Add(new Category{CategoryId =2 , Name= "Bilgisayar"});
+
+            _products.Add(new Product {ProductId= 1,Name = "Iphone 14",Price= 40000,IsActive= true,Image= "1.jpeg", CategoryId=1});
+            _products.Add(new Product {ProductId= 2,Name = "Iphone 15",Price= 50000,IsActive= true,Image= "3.jpeg", CategoryId=1});
+            _products.Add(new Product {ProductId= 3,Name = "Iphone 16",Price= 60000,IsActive= true,Image= "4.jpeg", CategoryId=1});
+            _products.Add(new Product {ProductId= 4,Name = "Iphone 17",Price= 70000,IsActive= true,Image= "5.jpeg", CategoryId=1});
+
+            _products.Add(new Product {ProductId= 5,Name = "Macbook Air",Price= 80000,IsActive= true,Image= "5.jpeg", CategoryId=2});
+            _products.Add(new Product {ProductId= 6,Name = "Macbook Pro",Price= 90000,IsActive= true,Image= "5.jpeg", CategoryId=2});
         }
 
         public static List<Product> Products
