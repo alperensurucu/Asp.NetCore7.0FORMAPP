ab7e83b3d7f96999a23c648be80d95549f54c5ba
alper <alperensurucu.eng@gmail.com>
2024-03-27 00:01:46 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetCore7._0FORMAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
